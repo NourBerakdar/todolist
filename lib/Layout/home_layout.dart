5b88cf05fbc343169e49a8f0f31db303821a80b4
NourHythamBerakdar <nour.berakdar@iotechnoware.com>
2024-02-16 06:44:59 +0300

    change date task and color of app bar

--- lib/Layout/home_layout.dart
+++ lib/Layout/home_layout.dart
@@ -7,33 +7,29 @@ import 'package:sqflite/sqflite.dart';
 import 'package:todo_app/shared/cubit/cubit.dart';
 import 'package:todo_app/shared/cubit/states.dart';
 
-
 class HomeLayout extends StatelessWidget {
-
-
   var scaffoldKey = GlobalKey<ScaffoldState>();
   var formKey = GlobalKey<FormState>();
   var titleController = TextEditingController();
   var dateController = TextEditingController();
   var timeController = TextEditingController();
 
-
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (BuildContext context)=>AppCubit()..createDataBase(),
-      child: BlocConsumer<AppCubit,AppStates>(
-        listener:(BuildContext context,AppStates state){
-          if(state is AppInsertDatabaseState){
+      create: (BuildContext context) => AppCubit()..createDataBase(),
+      child: BlocConsumer<AppCubit, AppStates>(
+        listener: (BuildContext context, AppStates state) {
+          if (state is AppInsertDatabaseState) {
             Navigator.pop(context);
           }
-        } ,
-        builder:(BuildContext context,AppStates state){
-          AppCubit cubit=AppCubit.get(context);
+        },
+        builder: (BuildContext context, AppStates state) {
+          AppCubit cubit = AppCubit.get(context);
           return Scaffold(
             appBar: AppBar(
               title: Text(cubit.Titles[cubit.currentIndex]),
+              backgroundColor: Color(0xFFD2B9FF),
             ),
             body: ConditionalBuilder(
               condition: state is! AppGetDatabaseLoadingState,
@@ -45,95 +41,103 @@ class HomeLayout extends StatelessWidget {
               onPressed: () {
                 if (cubit.isBottomSheet) {
                   if (formKey.currentState!.validate()) {
-                  cubit.insertToDataBase(title: titleController.text, time: timeController.text, date: dateController.text);
-                  titleController.clear();timeController.clear();dateController.clear();
+                    cubit.insertToDataBase(
+                        title: titleController.text,
+                        time: timeController.text,
+                        date: dateController.text);
+                    titleController.clear();
+                    timeController.clear();
+                    dateController.clear();
                   }
                 } else {
                   scaffoldKey.currentState
                       ?.showBottomSheet(
                           (context) => Container(
-                        color: Colors.white,
-                        padding: const EdgeInsets.all(20.0),
-                        child: Form(
-                          key: formKey,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              TextFormField(
-                                controller: titleController,
-                                validator: (String? value) {
-                                  return (value!.isEmpty)
-                                      ? 'title must not empty'
-                                      : null;
-                                },
-                                decoration: const InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: 'Task Title',
-                                  icon: Icon(Icons.title),
-                                ),
-                              ),
-                              const SizedBox(height: 15.0),
-                              TextFormField(
-                                onTap: () {
-                                  showTimePicker(
-                                      context: context,
-                                      initialTime: TimeOfDay.now())
-                                      .then((value) {
-                                    timeController.text =
-                                        value!.format(context).toString();
-                                  });
-                                },
-                                controller: timeController,
-                                validator: (String? value) {
-                                  return (value!.isEmpty)
-                                      ? 'time must not empty'
-                                      : null;
-                                },
-                                keyboardType: TextInputType.datetime,
-                                decoration: const InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: 'Task Time',
-                                  icon: Icon(Icons.watch_later_outlined),
-                                ),
-                              ),
-                              const SizedBox(height: 15.0),
-                              TextFormField(
-                                onTap: () async {
-                                  DateTime? date = DateTime(1900);
-                                  FocusScope.of(context)
-                                      .requestFocus(FocusNode());
+                                color: Colors.white,
+                                padding: const EdgeInsets.all(20.0),
+                                child: Form(
+                                  key: formKey,
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      TextFormField(
+                                        controller: titleController,
+                                        validator: (String? value) {
+                                          return (value!.isEmpty)
+                                              ? 'title must not empty'
+                                              : null;
+                                        },
+                                        decoration: const InputDecoration(
+                                          border: OutlineInputBorder(),
+                                          labelText: 'Task Title',
+                                          icon: Icon(Icons.title),
+                                        ),
+                                      ),
+                                      const SizedBox(height: 15.0),
+                                      TextFormField(
+                                        onTap: () {
+                                          showTimePicker(
+                                                  context: context,
+                                                  initialTime: TimeOfDay.now())
+                                              .then((value) {
+                                            timeController.text = value!
+                                                .format(context)
+                                                .toString();
+                                          });
+                                        },
+                                        controller: timeController,
+                                        validator: (String? value) {
+                                          return (value!.isEmpty)
+                                              ? 'time must not empty'
+                                              : null;
+                                        },
+                                        keyboardType: TextInputType.datetime,
+                                        decoration: const InputDecoration(
+                                          border: OutlineInputBorder(),
+                                          labelText: 'Task Time',
+                                          icon:
+                                              Icon(Icons.watch_later_outlined),
+                                        ),
+                                      ),
+                                      const SizedBox(height: 15.0),
+                                      TextFormField(
+                                        onTap: () async {
+                                          DateTime? date = DateTime(1900);
+                                          FocusScope.of(context)
+                                              .requestFocus(FocusNode());
 
-                                  date = await showDatePicker(
-                                    context: context,
-                                    initialDate: DateTime.now(),
-                                    firstDate: DateTime.now(),
-                                    lastDate: DateTime.parse("2022-12-31"),
-                                  );
-                                  dateController.text =
-                                      DateFormat.yMMMd().format(date!);
-                                },
-                                controller: dateController,
-                                validator: (String? value) {
-                                  return (value!.isEmpty)
-                                      ? 'date must not empty'
-                                      : null;
-                                },
-                                keyboardType: TextInputType.datetime,
-                                decoration: const InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: 'Task Date',
-                                  icon: Icon(Icons.calendar_today),
+                                          date = await showDatePicker(
+                                            context: context,
+                                            initialDate: DateTime.now(),
+                                            firstDate: DateTime.now(),
+                                            lastDate:
+                                                DateTime.parse("2030-12-31"),
+                                          );
+                                          dateController.text =
+                                              DateFormat.yMMMd().format(date!);
+                                        },
+                                        controller: dateController,
+                                        validator: (String? value) {
+                                          return (value!.isEmpty)
+                                              ? 'date must not empty'
+                                              : null;
+                                        },
+                                        keyboardType: TextInputType.datetime,
+                                        decoration: const InputDecoration(
+                                          border: OutlineInputBorder(),
+                                          labelText: 'Task Date',
+                                          icon: Icon(Icons.calendar_today),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      elevation: 20.0)
+                          elevation: 20.0)
                       .closed
                       .then((value) {
-                    cubit.changeBottomSheetState(isShow: false, icon: Icons.edit);
-
+                    cubit.changeBottomSheetState(
+                        isShow: false, icon: Icons.edit);
                   });
                   cubit.changeBottomSheetState(isShow: true, icon: Icons.add);
                 }
@@ -169,14 +173,10 @@ class HomeLayout extends StatelessWidget {
             ),
           );
         },
-
       ),
     );
   }
 
-
-
   Future<void> deleteDatabase(String path) =>
       databaseFactory.deleteDatabase(path);
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-      home:HomeLayout(),
+      debugShowCheckedModeBanner: false,
+      home: HomeLayout(),
     );
   }
 }
